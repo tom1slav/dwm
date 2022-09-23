d8fe03178934ff423acf8cec1ac7d4afe23a1dc3
Tomislav Nikic <tomislav.nikic@fhwn.ac.at>
2022-09-23 15:38:18 +0200

    adding swallow
    Disabling swallow for firefox and enabling it for st.

--- config.def.h
+++ config.def.h
@@ -2,15 +2,17 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 15;        /* gaps between windows */
+static const unsigned int gappx     = 15;       /* gaps between windows */
 static const unsigned int snap      = 35;       /* snap pixel */
+
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
 static const char *fonts[]          = {
 	"JetBrainsMono Nerd Font:size=14",
-	"Noto Sans CJK JP:size=14:style=heavy",
+	"Noto Sans CJK JP:size=14:style=bold",
 };
 
 /* duskfox */
@@ -49,9 +51,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Firefox", NULL,     NULL,           0,         0,          0,          -1,        -1 },
+	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 };
 
 /* layout(s) */
